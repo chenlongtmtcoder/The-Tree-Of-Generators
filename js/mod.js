593636e5fa8154108fa7bdc0c5c793616f4d4126
chenlongtmtcoder <butverybadmangolong@gmail.com>
2026-08-23 15:10:24 +0700

    hi

--- js/mod.js
+++ js/mod.js
@@ -20,13 +20,13 @@ let changelog = `<h1>Changelog:</h1><br>
 	<h2>v0.1</h2><br>
 		- Added <h3>1</h3> new layer, <b>Small Numbers</b><br>
 		- Added <b>10</b> upgrades.<br>
-		- Added <b>1</b> milestone<br>
-		- Endgame: Reach <b>1</b> and <b>5,000</b> Small Numbers<br><br>
+		- Added <b>1</b> milestone.<br>
+		- Endgame: Reach <h3>1</h3> and <b>5,000</b> Small Numbers<br><br>
 	<h2>v0.0</h2><br>
 		- Added things.<br>
 		- Added stuff.`
 
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+let winText = `You have reached the number limit! But for now... more comming soon!`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
